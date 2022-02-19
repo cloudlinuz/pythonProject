437d3a41fc48ef7bd8a96fd079737bb22359387f
admin <admin>
2022-02-19 20:10:03 +0530

    Variables

---
+++ variables.py
@@ -0,0 +1,12 @@
+# Variables in python
+
+x = input("Please enter x value? ")
+y = input("Please enter y value? ")
+x_value = float(x)
+y_value = float(y)
+print(f"Addition of x and y is {x_value + y_value}")
+print(f"Subtraction of x and y is {x_value - y_value}")
+print(f"Square of x and y is {x_value ** y_value}")
+print(f"Multiplication of x and y is {x_value * y_value}")
+print(f"Modular of x and y is {x_value % y_value}")
+print(f"Division of x and y is {round(x_value / y_value, 2)}")
