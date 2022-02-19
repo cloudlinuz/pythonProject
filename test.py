c62399c47c480561fb6085cfb8d4be222fb5f5d2
admin <admin>
2022-02-19 19:45:24 +0530

    Tip calculation

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ print("Welcome to the tip calculator1")
 bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
 people = int(input("How many people to split the bill? "))
-
+#
 tip_as_percent = tip / 100
 total_tip_amount = bill * tip_as_percent
 total_bill = bill + total_tip_amount
